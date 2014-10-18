d346fb222bd6d94ac1e353f2506d0e9e8d994f87
Tevin <tevin.williams773@gmail.com>
2014-10-18 14:19:22 -0400

    Can Pick Up Clues

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,24 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
-        public float Speed = 5f;
-
-	// Use this for initialization
-	void Start () {
+public class Player : MonoBehaviour 
+{
 	
-	}
+	public float Speed = 5f;
+	// Use this for initialization
 	
 	// Update is called once per frame
-	void Update () {
-
-            rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * Speed, 0.8f),
-                                                   Mathf.Lerp(0, Input.GetAxis("Vertical") * Speed, 0.8f));
+	void Update () 
+	{
+		rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * Speed, 0.8f),
+		                                   Mathf.Lerp(0, Input.GetAxis("Vertical") * Speed, 0.8f));
 	}
-
-        void OnTriggerEnter2D(Collider2D obj)
-        {
-            //print("Hit");
-        }
+	
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		if (gameObject.tag == "Clue")
+		{
+			Destroy(gameObject);
+		}
+	}
 }
