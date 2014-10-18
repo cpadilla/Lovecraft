3367586b8d9de99635ddf0e738ff7ef093d8c830
Etarip <etariple@gmail.com>
2014-10-17 20:02:42 -0400

    AI
    Ai and tag

---
+++ Assets/purpleAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class purpleAI : MonoBehaviour {
+	float speed = 3f;
+	
+	
+	///Inizialization///
+	function Start ()
+	{
+		enemy = GameObject.FindGameObjectsWithTag("Enemy");
+		player = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	
+	///Fixed Update///
+	function FixedUpdate ()
+	{
+		//Vector2.Distance = enemy.transform.position - player.transform.position
+		range = Vector2.Distance(enemy.transform.position, player.transform.position);
+		if(range <= 15f)
+		{
+			transform.Translate(Vector2.MoveTowards(enemy.transform.position, player.transform.position, range) * speed * Time.deltaTime);
+		}
+	}
+}
