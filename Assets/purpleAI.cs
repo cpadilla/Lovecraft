b629b62439c956bda4c1e8b5aaf66d713b782c7c
Christofer Padilla <christoferpadilla@gmail.com>
2014-10-17 20:09:14 -0400

    fixing purpleAI

--- Assets/purpleAI.cs
+++ Assets/purpleAI.cs
@@ -6,21 +6,21 @@ public class purpleAI : MonoBehaviour {
 	
 	
 	///Inizialization///
-	function Start ()
+	void Start ()
 	{
-		enemy = GameObject.FindGameObjectsWithTag("Enemy");
-		player = GameObject.FindGameObjectsWithTag("Player");
+        //enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        //player = GameObject.FindGameObjectsWithTag("Player");
 	}
 	
 	
 	///Fixed Update///
-	function FixedUpdate ()
+	void FixedUpdate ()
 	{
 		//Vector2.Distance = enemy.transform.position - player.transform.position
-		range = Vector2.Distance(enemy.transform.position, player.transform.position);
-		if(range <= 15f)
-		{
-			transform.Translate(Vector2.MoveTowards(enemy.transform.position, player.transform.position, range) * speed * Time.deltaTime);
-		}
+        //range = Vector2.Distance(enemy.transform.position, player.transform.position);
+        //if(range <= 15f)
+        //{
+        //    transform.Translate(Vector2.MoveTowards(enemy.transform.position, player.transform.position, range) * speed * Time.deltaTime);
+        //}
 	}
 }
