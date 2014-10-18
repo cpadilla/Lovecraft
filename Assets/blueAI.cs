4ffcdfc77244098e3a8bf6d56db0ee0689435f3c
Etarip <etariple@gmail.com>
2014-10-17 21:27:52 -0400

    New AI
    New AI

---
+++ Assets/blueAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blueAI : MonoBehaviour {
+	public float speed = 2f;
+	public float Distance = 10f;
+	float Travel = 0;
+	float rotate = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (Distance > Travel) {
+			transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+			Travel+=speed;
+		} else {
+			transform.Rotate (new Vector3 (0, 0, -3), Space.Self);
+			transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+			rotate += 3;
+			if (rotate == 90){
+				Travel = 0;
+				rotate = 0;
+			}
+		}
+	}
+}
--- Assets/purpleAI.cs
+++ Assets/purpleAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class purpleAI : MonoBehaviour {
 	public Transform target;//set target from inspector instead of looking in Update
-	public float speed = 3f;
+	public float speed = 2f;
 	
 	
 	///Inizialization///
@@ -20,7 +20,7 @@ public class purpleAI : MonoBehaviour {
 		transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
 
 		//move towards the player
-		if (Vector3.Distance(transform.position,target.position)>1f){//move if distance from target is greater than 1
+		if (Vector3.Distance(transform.position,target.position)>0.5f){
 			transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
 		}
 	}
