13f378a1fe3b45b11f10b9c38aca214f999b43ff
Etarip <etariple@gmail.com>
2014-10-17 23:29:19 -0400

    Last tonight
    last tonight

--- Assets/purpleAI.cs
+++ Assets/purpleAI.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class purpleAI : MonoBehaviour {
 	public Transform target;//set target from inspector instead of looking in Update
 	public float speed = 2f;
-	
-	
+	//int count=0;
 	///Inizialization///
 	void Start ()
 	{
@@ -16,12 +15,36 @@ public class purpleAI : MonoBehaviour {
 	///Fixed Update///
 	void Update ()
 	{
+
 		transform.LookAt(target.position);
 		transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
 
-		//move towards the player
 		if (Vector3.Distance(transform.position,target.position)>0.5f){
+			transform.Translate(new Vector3(speed* Time.deltaTime,0,0));
+		}
+		//move towards the player
+		/*
+		if (Vector3.Distance(transform.position,target.position)>0.5f){
+			if(count<50){
+				Vector3 temp = target.position;
+				temp.x+=1;
+				temp.y+=1;
+				Vector3 dir = temp - transform.position;
+				float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+				count++;
+			}else{
+				Vector3 temp = target.position;
+				temp.x-=1;
+				temp.y-=1;
+				Vector3 dir = temp - transform.position;
+				float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+				count++;
+				if(count>=100) count=0;
+			}
 			transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
 		}
+		*/
 	}
 }
