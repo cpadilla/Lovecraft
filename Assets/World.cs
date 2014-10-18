562e6966aaa1d1c7d7597eff406dfc9142cafc2d
Christofer Padilla <christoferpadilla@gmail.com>
2014-10-17 20:57:49 -0400

    Updated WorldTile

--- Assets/World.cs
+++ Assets/World.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class World : MonoBehaviour {
+public partial class World : MonoBehaviour {
+
+        //List<Plane> 
+        public GameObject Tile;
+        WorldTile start = new WorldTile();
 
 	// Use this for initialization
 	void Start () {
-	
+            GameObject startTile = (GameObject)Instantiate(Tile, transform.position, transform.rotation);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/WorldTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldTile : MonoBehaviour {
+
+        public WorldTile Up;
+        public WorldTile Down;
+        public WorldTile Left;
+        public WorldTile Right;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
