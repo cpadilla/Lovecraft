5782f99c56ab20f2dad0999b6fbd051187dd9253
Christofer Padilla <christoferpadilla@gmail.com>
2014-10-17 22:30:57 -0400

    Going places!

---
+++ Assets/GoNorth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoNorth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            
+	}
+
+        void OnTriggerEnter2D(Collider2D obj)
+        {
+            if (obj.tag == "Player")
+            {
+                print("Player went North!");
+            }
+        }
+}
