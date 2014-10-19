9434bc46c5fcb616473a62b1a13900af78e9255d
Etarip <etariple@gmail.com>
2014-10-19 15:32:07 -0400

    Health Bar
    HealthBar

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class HealthBar : MonoBehaviour {
-
+	float x=0f,y=0f;
+	Transform position;
 	// Use this for initialization
 	void Start () {
-		
+		position = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.position = new Vector3(8 * Player.location.X - 3.1f, 6 * Player.location.Y - 2.7f, 0);
 		int children = transform.childCount;
 		GameObject[] list = new GameObject[children];
 		for (int i = 0; i < children; ++i)
